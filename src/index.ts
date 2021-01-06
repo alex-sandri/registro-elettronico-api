e30c19a00d83bff37f8d3fc0e15317df7f83f9b7
Alex Sandri <me@alexsandri.com>
2021-01-06 18:24:37 +0100

    Install other graphql related deps

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import * as express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
+
+const schema = buildSchema(`
+    type Query
+    {
+        hello: String
+    }
+`);
+
+const root = {
+    hello: () => {
+      return 'Hello world!';
+    },
+};
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
+app.listen(4000);
