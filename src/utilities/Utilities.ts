fa54e5beab189e3527d81d7abac53f59bac21cf4
Alex Sandri <me@alexsandri.com>
2021-01-29 18:37:52 +0100

    Implement generateEmailAddress utility

--- src/utilities/Utilities.ts
+++ src/utilities/Utilities.ts
@@ -1,4 +1,5 @@
 import { hashSync, compareSync } from "bcrypt";
+import { Config } from "../config/Config";
 
 export default class Utilities
 {
@@ -10,5 +11,34 @@ export default class Utilities
     public static verifyHash(data: string, hash: string): boolean
     {
         return compareSync(data, hash);
+    }
+
+    public static generateEmailAddress(firstName: string, lastName: string): string
+    {
+        let username = "";
+
+        switch (Config.EMAIL_AUTO_GEN_MODE)
+        {
+            case "first.last":
+            {
+                username = `${firstName}.${lastName}`;
+
+                break;
+            }
+            case "f.last":
+            {
+                username = `${firstName.charAt(0)}.${lastName}`;
+
+                break;
+            }
+            case "flast":
+            {
+                username = `${firstName.charAt(0)}${lastName}`;
+
+                break;
+            }
+        }
+
+        return `${username}@${Config.EMAIL_DOMAIN}`;
     }
 }
