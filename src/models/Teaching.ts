aa1a396db17b7707c8c65dba49f79273264cc615
Alex Sandri <me@alexsandri.com>
2021-02-01 09:40:03 +0100

    Migrate Teaching.ts to raw SQL queries

--- src/models/Teaching.ts
+++ src/models/Teaching.ts
@@ -1,5 +1,6 @@
 import { ISerializable } from "../common/ISerializable";
 import Database from "../utilities/Database";
+import Utilities from "../utilities/Utilities";
 import Class, { ISerializedClass } from "./Class";
 import Subject, { ISerializedSubject } from "./Subject";
 import Teacher, { ISerializedTeacher } from "./Teacher";
@@ -57,39 +58,25 @@ export default class Teaching implements ISerializable
             throw new Error("This teacher does not exist");
         }
 
-        const teaching = await db.teaching.create({
-            data: {
-                Class: {
-                    connect: {
-                        name: data.class,
-                    },
-                },
-                Subject: {
-                    connect: {
-                        name: data.subject,
-                    },
-                },
-                Teacher: {
-                    connect: {
-                        email: data.teacher,
-                    },
-                },
-            },
-        });
+        const id = Utilities.id();
 
-        return new Teaching(teaching.id, data);
+        await db.query(
+            "insert into teachings (id, class, subject, teacher) values ($1, $2, $3)",
+            [ id, data.class, data.subject, data.teacher ],
+        );
+
+        return new Teaching(id, data);
     }
 
     public static async for(teacher: Teacher): Promise<Teaching[]>
     {
         const db = Database.client;
 
-        const teachings = await db.teaching.findMany({
-            where: {
-                teacher: teacher.data.email,
-            },
-        });
+        const result = await db.query(
+            "select * from teachings where teacher = $1",
+            [ teacher.data.email ],
+        );
 
-        return teachings.map(_ => new Teaching(_.id, _));
+        return result.rows.map(_ => new Teaching(_.id, _));
     }
 }
